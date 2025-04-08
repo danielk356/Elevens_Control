81efc3aa5cf1921455512e667c3d74d2d021a8df
BT_2S14_33 <danielk356@nycstudents.net>
2025-04-08 10:13:28 -0400

    displayed 3 by 3 grid

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -15,16 +15,16 @@ class DrawPanel extends JPanel implements MouseListener {
     private Rectangle button;
 
     public DrawPanel() {
-        button = new Rectangle(147, 100, 160, 26);
+        button = new Rectangle(155, 260, 160, 26);
         this.addMouseListener(this);
         hand = Card.buildHand();
     }
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        int x = 50;
+        int x = 130;
         int y = 10;
-        for (int i = 0; i < hand.size(); i++) {
+        /* for (int i = 0; i < hand.size(); i++) {
             Card c = hand.get(i);
             if (c.getHighlight()) {
                 // draw the border rectangle around the card
@@ -36,11 +36,32 @@ class DrawPanel extends JPanel implements MouseListener {
 
             g.drawImage(c.getImage(), x, y, null);
             x = x + c.getImage().getWidth() + 10;
+        } */
+
+        int index = 0;
+        for (int row = 0; row < 3; row++) {
+            Card c = hand.get(index);
+            for (int col = 0; col < 3; col++) {
+                c = hand.get(index);
+                if (c.getHighlight()) {
+                    // draw the border rectangle around the card
+                    g.drawRect(x, y, c.getImage().getWidth(), c.getImage().getHeight());
+                }
+
+                // establish the location of the rectangle "hitbox"
+                c.setRectangleLocation(x, y);
+
+                g.drawImage(c.getImage(), x, y, null);
+                x = x + c.getImage().getWidth() + 10;
+                index++;
+            }
+            x = 130;
+            y = y + c.getImage().getHeight() + 20;
         }
 
         // drawing the bottom button
         g.setFont(new Font("Courier New", Font.BOLD, 20));
-        g.drawString("GET NEW CARDS", 150, 120);
+        g.drawString("GET NEW CARDS", 158, 279);
         g.drawRect((int)button.getX(), (int)button.getY(), (int)button.getWidth(), (int)button.getHeight());
     }
 
